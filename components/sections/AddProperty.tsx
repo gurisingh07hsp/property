d68779cab3471b499a6754650e76635860781eb2
drmac <drmac@192.168.1.6>
2026-06-02 21:38:23 +0530

    update project

--- components/sections/AddProperty.tsx
+++ components/sections/AddProperty.tsx
@@ -12,7 +12,7 @@ export default function AddProperty() {
                                 <h3>Add New Property</h3>
                                 <div className="space40" />
                                 <div className="all-tabs-boxarea">
-                                    <ul className="nav nav-pills" id="pills-tab" role="tablist">
+                                    <ul className="nav nav-pills add-property-steps-nav" id="pills-tab" role="tablist">
                                         <li className="nav-item" role="presentation">
                                             <button className="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">
                                                 1. Description
@@ -351,7 +351,14 @@ export default function AddProperty() {
                                                         <div className="space48" />
                                                         <div className="mapouter">
                                                             <div className="gmap_canvas">
-                                                                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d4506257.120552435!2d88.67021924228865!3d21.954385721237916!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1704088968016!5m2!1sen!2sbd" width={600} height={450} style={{ border: 0 }} allowFullScreen={true} loading="lazy" referrerPolicy="no-referrer-when-downgrade" />
+                                                                <iframe
+                                                                    src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d4506257.120552435!2d88.67021924228865!3d21.954385721237916!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1704088968016!5m2!1sen!2sbd"
+                                                                    title="Property location map"
+                                                                    style={{ border: 0, width: "100%", height: "100%", minHeight: 220 }}
+                                                                    allowFullScreen
+                                                                    loading="lazy"
+                                                                    referrerPolicy="no-referrer-when-downgrade"
+                                                                />
                                                             </div>
                                                         </div>
                                                     </div>
